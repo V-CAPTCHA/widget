fadd0e4e36e7b4c52fbf194d297a8d81bbbd38ef
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-10-31 04:06:56 +0700

    work with real question for captcha dialog

--- vcaptchaWidget.js
+++ vcaptchaWidget.js
@@ -4,33 +4,21 @@ var recognition = new SpeechRecognition();
 recognition.lang = 'th-TH';
 var transcript;
 var vcaptcha;
-
-
-
+var c_Id;
+var c_Dataset;
+var c_DatasetQuestion;
 
 function init() {
-
-
-
   vcaptcha = document.getElementsByTagName('vcaptcha')[0];
   //vcaptcha.appendChild(createForms(vcaptcha));
   vcaptcha.appendChild(createDialog(vcaptcha));
-
-  
- 
-  
 }
-
-
-
-
-
 
 function createDialog() {
   let parent = document.createElement('div'); //create parent div for content
 
   // !!!Setup Div Dialog!!!
-  parent.id='dialogActionVcaptcha';
+  parent.id = 'dialogActionVcaptcha';
   parent.style.display = 'block';
   parent.style.width = '200px';
   parent.style.font = '12px Helvetica';
@@ -38,11 +26,7 @@ function createDialog() {
   parent.style.borderRadius = '3px';
   parent.style.padding = '10px';
   parent.style.margin = 'auto';
-  parent.style.marginBottom="20px";
-  
-
-  
-  
+  parent.style.marginBottom = '20px';
 
   //Dialog Button
   let DialogButton = document.createElement('button');
@@ -54,7 +38,7 @@ function createDialog() {
   DialogButton.style.border = 'None';
   DialogButton.style.width = '100%';
   DialogButton.style.height = '35px';
- 
+
   parent.appendChild(DialogButton);
   return parent;
 }
@@ -63,7 +47,7 @@ function createForms() {
   let parent = document.createElement('div'); //create parent div for content
 
   // !!!Setup Div Prop!!!
-  parent.id='formVcaptcha';
+  parent.id = 'formVcaptcha';
   parent.style.display = 'block';
   parent.style.width = '200px';
   parent.style.font = '12px Helvetica';
@@ -71,30 +55,29 @@ function createForms() {
   parent.style.borderRadius = '3px';
   parent.style.padding = '10px';
   parent.style.margin = 'auto';
-  parent.style.marginBottom="20px"
-  
+  parent.style.marginBottom = '20px';
 
   // !!!Config block content for child prop!!!
 
   //question name
   let question = document.createElement('h2');
-  question.innerHTML = '"คำถาม"'; //question name
+  question.innerHTML = c_DatasetQuestion; //question name
   question.style.textAlign = 'center';
 
   //image for question
   let img = document.createElement('img');
-  img.src = 'https://dataset.vcaptcha.work/q1.jpg';
+  img.src = 'https://dataset.vcaptcha.work/q'+c_Dataset+'.jpg';
   img.style.maxWidth = '100%';
 
   //Respone Text
   let responeUser = document.createElement('h3');
-  responeUser.innerHTML = '"คำตอบ"';
+  responeUser.innerHTML = '"พูดเพื่อตอบคำถาม"';
   responeUser.style.textAlign = 'center';
-  responeUser.id="txtRespone"; //set ID for get voice recog text
+  responeUser.id = 'txtRespone'; //set ID for get voice recog text
 
   //text guide name
   let textGuide = document.createElement('p');
-  textGuide.innerHTML = 'พูดเพื่อตอบคำถาม';
+  textGuide.innerHTML = 'เรากำลังฟังคุณอยู่...';
   textGuide.style.textAlign = 'center';
 
   // !!!Setup Div Prop!!!
@@ -110,52 +93,66 @@ function createForms() {
 
 //Voice Recog Function
 
-recognition.onresult = function(event) {
-    transcript = event.results[0][0].transcript;
-    document.getElementById("txtRespone").innerHTML=transcript;
+recognition.onresult = function (event) {
+  transcript = event.results[0][0].transcript;
+  document.getElementById('txtRespone').innerHTML = transcript;
 };
-recognition.onstart = function() {
-    console.log("SpeechRecognition is Starting..");
+recognition.onstart = function () {
+  console.log('SpeechRecognition is Starting..');
 };
 
-recognition.onspeechend = function() {
-    // when user is done speaking
-    recognition.stop();
-}
+recognition.onspeechend = function () {
+  // when user is done speaking
+  recognition.stop();
+};
 
 
 
-function debug_api(){
-  valueDomain ='clienttest.com';
+function getCaptcha_api() {
+  valueDomain = 'clienttest.com';
   valueKey = '1150123vcaptcha';
-  fetch('http://widgetapi.vcaptcha.work/GetCaptcha?domain='+ valueDomain +'&key='+valueKey).then(function (response) {
-	// The API call was successful!
-	if (response.ok) {
-		return response.json();
-	} else {
-		return Promise.reject(response);
-	}
-}).then(function (data) {
-	// This is the JSON from our response
-  alert(data);
-	console.log(data);
-}).catch(function (err) {
-	// There was an error
-	console.warn('Something went wrong.', err);
-});
+  fetch(
+    'http://widgetapi.vcaptcha.work/GetCaptcha?domain=' +
+      valueDomain +
+      '&key=' +
+      valueKey
+  )
+    .then(function (response) {
+      // The API call was successful!
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (data) {
+      // This is the JSON from our response
+      c_Id = data.action_id;
+      c_Dataset = data.dataset_img;
+      c_DatasetQuestion = data.dataset_question;
+      vcaptcha.appendChild(createForms(vcaptcha)) //call captcha dialog
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn('Something went wrong.', err);
+    });
 }
 
 //Addonn Function
 
-document.getElementById("buttonActionVcaptcha").addEventListener("mouseover", function() {
-  this.style.backgroundColor = 'rgb(41 105 190)';
-});
-document.getElementById("buttonActionVcaptcha").addEventListener("mouseout", function() {
-  this.style.backgroundColor = '#1a73e8';
-});
+document
+  .getElementById('buttonActionVcaptcha')
+  .addEventListener('mouseover', function () {
+    this.style.backgroundColor = 'rgb(41 105 190)';
+  });
+document
+  .getElementById('buttonActionVcaptcha')
+  .addEventListener('mouseout', function () {
+    this.style.backgroundColor = '#1a73e8';
+  });
 
-buttonActionVcaptcha.addEventListener('click', function(event) {
-  vcaptcha.appendChild(createForms(vcaptcha));
-  const div = document.getElementById("dialogActionVcaptcha");
-div.remove();
+buttonActionVcaptcha.addEventListener('click', function (event) {
+  getCaptcha_api();
+  const div = document.getElementById('dialogActionVcaptcha');
+  div.remove();
 });
